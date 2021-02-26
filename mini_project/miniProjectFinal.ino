8c868d56ea31e19f6d155f636b4aea4f07502903
samuelrbair <79220599+samuelrbair@users.noreply.github.com>
2021-02-25 20:33:21 -0700

    Add files via upload

---
+++ mini_project/miniProjectFinal.ino
@@ -0,0 +1,130 @@
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x04
+byte number = 0;
+int state = 0;
+byte data[32] = {0};
+
+
+#include <Encoder.h>
+Encoder myEnc(2, 6);
+
+#define D2 4
+#define M1DIR 7
+#define M2DIR 8
+#define M1PWM 9
+#define M2PWM 10
+#define SF 12
+
+
+float kp = 0.62;//0.6275;
+float ki = 0.19;//0.024, increased a lot cuz we want wind up -> ensures we don't stop before the position we want -> increase for better transient response
+float integrator = 0;
+float e = 0;
+float ts = 0;
+float tc = millis();
+float r = 0; 
+float y = 0;
+int u = 0; //controller (volts)
+
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(115200); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+  //Serial.println("Ready!");
+
+  pinMode(D2, OUTPUT);
+  pinMode(M1DIR, OUTPUT);
+  pinMode(M2DIR,OUTPUT);
+  pinMode(M1PWM, OUTPUT);
+  pinMode(M2PWM, OUTPUT);
+  pinMode(SF, INPUT);
+  digitalWrite(D2,HIGH);
+  
+  myEnc.write(0);
+
+  Serial.println("Ready!");
+  
+}
+
+void loop() {
+      y=myEnc.read();
+      y = (y/1600)*PI;
+      e=r-y;//difference in position or error
+      //Serial.print(y);
+      //Serial.print("\t");
+      integrator = integrator + ts*e; //might need to divide by 1000
+      u = kp*e+ki*integrator*e; //controller(voltage)
+      Serial.println(u);
+     
+      u = u/7.9 *255; //this may be funky but shouldn't matter too much
+      //check if u is overflowing (greater or less than 255)
+      if(u > 255){
+        u=255;
+      }else if(u < -255){
+        u = 255;
+      }
+      analogWrite(M1PWM, abs(u));//255 is 5V
+      
+      if(u<0){
+        digitalWrite(M1DIR, LOW);
+      }else{
+        digitalWrite(M1DIR, HIGH);
+      }
+
+      ts = millis() - tc;
+      tc = millis();
+      //Serial.print(tc);
+      //Serial.print("\t");
+      //Serial.println();
+}
+
+// callback for received data
+void receiveData(int byteCount){
+    int i=0;
+    
+    while(Wire.available()) {
+     data[i] = Wire.read();
+     if(data[i]==5)break;
+     Serial.print(data[i]);
+     Serial.print(' ');
+     i++;
+    }
+    //Serial.println(' ');
+    i--;
+    if(data[i]!= 5 && i < 31 && i > 0){
+      Serial.print("<- New data ");
+      r=data[i]*(PI/2);
+      Serial.println(r);
+    }
+}
+
+// callback for sending data
+void sendData(){
+  Serial.println("Sending...");
+  Serial.print(y);
+  /*
+  Serial.print(' ');
+  Serial.print(int((y*40)));
+  Serial.print(' ');
+  number=int((y*40));
+  Serial.println(number);
+  */
+  if (y == 0){
+    number = 0;
+  }else if(y > 0 && y < 2){
+    number = 1;
+  }else if( y < 4){
+    number = 2;
+  }else{
+    number = 3;
+  }
+  Wire.write(number);
+}
---
+++ mini_project/miniProjectFinal.py
@@ -0,0 +1,146 @@
+#importing packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+import smbus
+import board
+import busio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+
+
+def LCDDisplay(desired,actual):
+    lcd.clear()
+    lcd.color = [100, 100, 100]
+    #time.sleep(1)
+    time.sleep(0.1)
+    lcd.message = "Setpoint: " + str(desired) + "\nPostion: " + str(actual)
+    #lcd.color = [0, 0, 0]
+    #time.sleep(1)
+    time.sleep(0.1)
+    return 1
+
+def writeNumber(value):
+    #bus.write_byte(address, value)
+    if value == None:
+        return -2
+    try:
+        bus.write_byte_data(address, 0, value)
+    except:
+        return -1
+    
+    return 0
+
+def readNumber():
+    #number = bus.read_byte(address)
+    number = bus.read_byte_data(address, 5)
+    return number
+
+#continuously takes pictures and checks for aruco markers
+def FindQuadrant(corners, ids, WIDTH, HEIGHT):
+    #Check for Quadrent
+    if ids != None:
+        for marker in range(0,ids.size):
+            mark = np.array(corners[marker][0])
+
+            #x and y coordinate of center of marker
+            x = ((mark[1][0] - mark[0][0])/2) + mark[0][0]
+            y = ((mark[3][1] - mark[0][1])/2) + mark[0][1]
+            #determining the quadrant
+            if(x < (WIDTH/2) and y < (HEIGHT/2)):
+                #upper left quadrant
+                return 0
+            elif(x >= (WIDTH/2) and y < (HEIGHT/2)):
+                #upper right quadrant
+                return 1
+            elif(x >= (WIDTH/2) and y >= (HEIGHT/2)):
+                #lower right quadrant
+                return 2
+            else:
+                #lower left quadrant
+                return 3
+
+#MAIN PROGRAM
+
+# for RPI version 1, use “bus = smbus.SMBus(0)”
+bus = smbus.SMBus(1)
+
+# Initialise I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+lcd_columns = 16
+lcd_rows = 2
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+
+# This is the address we setup in the Arduino Program
+address = 0x04
+
+#initializing camera
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+time.sleep(0.1)
+camera.capture(rawCapture, format="bgr")
+
+#Setting up camera
+HEIGHT = 768
+WIDTH = 1024
+quadrant = None
+camera.resolution = (WIDTH, HEIGHT)
+rawCapture.truncate(0)
+
+#Creating aruco dictionary and parameters
+arucoDict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+arucoParam = aruco.DetectorParameters_create()
+    
+loop = True
+#taking pictures and checking for markers
+while loop:
+    try:
+        camera.capture(rawCapture,format='bgr')
+        img = rawCapture.array
+            
+        #converting to grayscale
+        grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+        #detecting markers
+        (corners, ids, rejected) = aruco.detectMarkers(grayImg, arucoDict, parameters=arucoParam)
+
+        #Find and return quadrant
+        quadrant = FindQuadrant(corners, ids, WIDTH, HEIGHT)
+
+        print(quadrant)
+
+        print(writeNumber(quadrant))
+        
+        if(quadrant != None):
+
+            #writeNumber(quadrant)
+            quadrant=quadrant*64
+            time.sleep(2)
+            newQuadrant = 0#readNumber() * 64
+            LCDDisplay(quadrant,newQuadrant)
+            #while(quadrant != (newQuadrant)):
+            #    time.sleep(1)
+            #    newQuadrant = readNumber()
+            #    print(newQuadrant)
+            #    LCDDisplay(quadrant,newQuadrant)
+                
+
+
+        #time.sleep(1)
+
+        #newQuadrant = readNumber()
+
+        #time.sleep(0.1)
+
+        
+        #   LCDDisplay(quadrant,newQuadrant)
+
+        rawCapture.truncate(0)
+    #exits loop when ctl+c is pressed
+    except KeyboardInterrupt:
+        loop = False
