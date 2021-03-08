0d773731da1e2cd032b9ab4ef1eaa2bd061aa072
samuelrbair <79220599+samuelrbair@users.noreply.github.com>
2021-03-08 11:53:20 -0700

    Add files via upload

---
+++ Demo1/MiniDemoLCD.py
@@ -0,0 +1,24 @@
+import smbus
+import time
+import board
+import busio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+#This is setup for the LCD Display
+bus = smbus.SMBus(1)
+i2c = busio.I2C(board.SCL, board.SDA)
+lcd_columns = 16
+lcd_rows = 2
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+
+#This is the I2C address of the arduino
+address = 0x04
+
+def LCDDisplayAngle(angle):
+    lcd.clear()
+    lcd.color = [100, 100, 100]
+    #time.sleep(1)
+    lcd.message = "MRKR Found " + "\nAngle: " + round(angle,2)
+    lcd.color = [0, 0, 0]
+    #time.sleep(1)
+    return 1
