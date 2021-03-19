1da6770d736b925173f17588c6620f2e640a9568
drbyles <79228145+drbyles@users.noreply.github.com>
2021-03-19 15:00:32 -0600

    Update MatlabCodeDemo1.m

--- Demo1/Controller/MatlabCodeDemo1.m
+++ Demo1/Controller/MatlabCodeDemo1.m
@@ -1,14 +1,14 @@
 %% Demo 1: Control Design and Method
 % My Demo 1 design process:
 % 
-% # Determine the angular velocity of each wheel 
+% # Determine the angular velocity of each wheel
 % # Convert angular velocity to gain
 % # Determine the transfer functions
 % #Create PI controllers and find kp and ki values
 % 
 
 %% Determining wheel one trasnfer function
-k1 = 0.04857657658; %rad/(volt*sec)
+k1 = 0.236; %rad/(volt*sec)
 omega1 = 100; 
 out = sim('transferFunctionWheel1');
 plot(out.wheel1);
@@ -16,33 +16,30 @@ title('Simulated Open Loop Response for 1st wheel');
 ylabel('Angular Velocity(rad/sec)');
 
 %% Determining wheel two transfer function
-k2 = 0.04885585586;
+k2 = 0.274;
 omega2 = 100; 
 out = sim('transferFunctionWheel2');
 plot(out.wheel2);
 title('Simulated Open Loop Response for 2nd wheel');
 ylabel('Angular Velocity(rad/sec)');
-
 %% Controller for Wheel1
-k1 = 0.04857657658; %rad/(volt*sec)
+k1 = 0.236; %rad/(volt*sec)
 omega1 = 100;
 out = sim('wheel1Controller');
 plot(out.controller1);
 title('Simulated Closed Loop Response: First Wheel');
 ylabel('Angular position(rad)');
-%P=615.397990431844
-%I=309.498975578068
-
+%P=13.72
+%I=0.777
 %% Controller for Wheel2
-k2 = 0.04885585586;
+k2 = 0.274;
 omega2 = 100; 
 out = sim('wheel2Controller');
 plot(out.controller2);
 title('Simulated Closed Loop Response: Second Wheel');
 ylabel('Angular position(rad)');
-%P=696.274989239527
-%I=394.448534034899
-
+%P=11.8178
+%I=0.66949
 %% Forward Velocity Transfer Function
 % Determining the forward velocity
 thetaRightWheel = 1.18;
@@ -55,7 +52,7 @@ omega = 100;
 out = sim('forwardTransferFunction');
 plot(out.forwardPositionTransferFcn);
 
-%% Forward Velocity Controller
+%% Forward Velocity Controller Porportinal
 % implementing P controller
 thetaRightWheel = 1.18;
 thetaLeftWheel = 1.37;
@@ -66,8 +63,7 @@ velocity = ((thetaRightWheel+thetaLeftWheel)*r)/2;
 omega = 100;
 out = sim('forwardControl');
 plot(out.forwardVelocity);
-%P = 186623
-
+%kP = 186623
 %% Angular Velocity Transfer Function
 % determing the angular velocity
 thetaRightWheel = 1.18;
@@ -81,16 +77,44 @@ omega = 1000;
 out = sim('angularTransferFunction');
 plot(out.angularTF);
 
-%% Angular Velocity Controller
+%% Angular Velocity Controller porportional
 % angular Velocity Controller
-thetaRightWheel = 1.18;
+thetaRightWheel = 1.18; %ms
 thetaLeftWheel = 1.37;
 v1 = .5; %voltage
 v2 = .5; %voltage
 d = 0.29; %distance between wheels
-r = 0.07485;
+r = 0.07485; %%diameter
 velocity = ((thetaRightWheel-thetaLeftWheel)/d)*r;
 omega = 1000;
 out = sim('angularController');
 plot(out.angularVelocity);
-%P = -363178.639861224
+%kP = -363178.639861224
+
+%% Forward Velocity Controller Integral
+% implementing PI controller
+thetaRightWheel = 1.18;
+thetaLeftWheel = 1.37;
+v1 = .5; %voltage
+v2 = .5; %voltage
+r= 0.07485; %in meters, will have to change 
+velocity = ((thetaRightWheel+thetaLeftWheel)*r)/2;
+omega = 100;
+out = sim('forwardControllerIntegrator');
+plot(out.forwardVelocity);
+%kI = 60;
+
+%% Angular Velocity Controller Integral
+% angular Velocity Controller
+thetaRightWheel = 1.18; %ms
+thetaLeftWheel = 1.37;
+v1 = .5; %voltage
+v2 = .5; %voltage
+d = 0.29; %distance between wheels
+r = 0.07485; %%diameter
+velocity = ((thetaRightWheel-thetaLeftWheel)/d)*r;
+velocity = abs(velocity);
+omega = 100;
+out = sim('angularControllerIntegrator');
+plot(out.angularVelocity);
+%ki= 117;
