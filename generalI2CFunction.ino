8491986f5fbf9001f34492e8d48afa7300da2386
samuelrbair <79220599+samuelrbair@users.noreply.github.com>
2021-03-26 11:29:37 -0600

    Update generalI2CFunction.ino

--- generalI2CFunction.ino
+++ generalI2CFunction.ino
@@ -3,8 +3,8 @@
 #define SLAVE_ADDRESS 0x04
 int number = 0;
 int state = 0;
-long longNumber = 0;
-float floatNumber = 0;
+unsigned long longValue = 0;
+float floatValue = 0;
 byte data[32] = {0};
 byte newdata[32] = {0};
 int i=0;
@@ -24,6 +24,22 @@ void setup() {
 
 void loop() {
     delay(100);
+    //data[0] = 195;
+    //data[1] = 245;
+    //data[2] = 72;
+    //data[3] = 64; 
+    //for(unsigned long j = 0; j<4; j++){
+    //  Serial.println(data[j]);
+    //  Serial.println((long(data[j]) << (j*8)));
+    //  longValue = (long(data[j]) << (j<<3)) | longValue;
+    //  Serial.println(longValue);
+    //  delay(1000);
+    //}
+    
+    //Serial.println(longValue);
+    //floatValue = *((float*)&longValue);
+    //Serial.println(floatValue);
+    
 }
 
 // callback for received data
@@ -34,42 +50,39 @@ void receiveData(int byteCount){
      data[i] = Wire.read();
      //Serial.print(data[i]);
      //Serial.print(' ');
-     if(i == 0 && data[i] > 126) break;
+     if(i == 0 && data[i] > 63) break;
      i++;
-     //stringSize++;
     }
     //Byte
     if(data[0] == 0){
         number = data[1];
+        Serial.println(number);
     //Long
     }else if(data[0] == 1){
       longValue = 0;
-      for(int j = i; j>=1; i--) longValue = (data[j] << ((j-1))<<3) | longValue;
+      for(byte j = 1; j<5; j++) longValue = (data[j] << ((j-1))<<3) | longValue;
     //Float
     }else if(data[0] == 2){
       longValue = 0;
-      for(int j = i; j>=1; i--) longValue = (data[j] << ((j-1))<<3) | longValue;
-      floatValue = * ( float *) &longValue; // evil floating point bit level hack
+      for(byte j = 1; j<5; j++) longValue = (long(data[j]) << ((j-1)<<3)) | longValue;
+      floatValue = *((float*)&longValue); // evil bit level hack
+      Serial.println(floatValue);
     }
     
 }
 
 // callback for sending data
 void sendData(){
-    //Serial.println(stringSize);
-    //stringSize=1;
-    //Wire.write(newdata,stringSize);
     if(data[0] == 64){
         Wire.write(number);
     //Long
     }else if(data[0] == 65){
-      
-      for(int j = 0; j < 4; i++) newdata[j] = (longValue & (0xFF << (i << 3)) >> (i << 3)
+      for(int j = 1; j < 5; j++) newdata[j] = byte(longValue & (0xFF << ((j-1) << 3)) >> ((j-1) << 3));
       Wire.write(newdata, 4);
     //Float
     }else if(data[0] == 66){
       long tempLong = * ( long *) &floatValue;
-      for(int j = 0; j < 4; i++) newdata[j] = (tempLong & (0xFF << (i << 3)) >> (i << 3)
+      for(int j = 1; j < 5; j++) newdata[j] = byte(tempLong & (0xFF << ((j-1) << 3)) >> ((j-1) << 3));
       Wire.write(newdata, 4);
     }
     
