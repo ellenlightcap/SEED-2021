5ead017849d46fe6664d2683a15503bcc3343c9e
Tyrone Vincent <tvincent@mines.edu>
2021-04-01 16:54:58 -0600

    Added Position Controller
    Reorganized startinggain folder to contain both velocity control (inner loop only) and position control (both inner and outer loop) folders for both matlab design and arduino implementation

---
+++ Demo1/startingagain/PositionControl/PositionController.ino
@@ -0,0 +1,75 @@
+void Controller() {
+   float rho_dot_error=0;
+   float phi_dot_error=0;
+   float PWM_bar=0;
+   float PWM_delta = 0;
+   int M1PWM_value=0;
+   int M2PWM_value=0;
+   float rho_error=0;
+   float rho_error_old = 0;
+   float phi_error=0;
+   float phi_error_old=0;
+   float rho_dot_setpoint_internal=0;
+   float phi_dot_setpoint_internal=0;
+
+   // set velocity setpoints to global variables in case velocity control is desired
+   rho_dot_setpoint_internal = rho_dot_setpoint;
+   phi_dot_setpoint_internal = phi_dot_setpoint;
+
+   // implement outer controllers to set velocity setpoint, if position control is desired
+   if (ANGULAR_POSITION_CONTROL) {
+        phi_error = phi_setpoint - phi;
+        phi_dot_setpoint= Kp_phi*phi_error + Kd_phi*(phi_error- phi_error_old)/((float)period/1000);
+        phi_error_old = phi_error;
+   }
+   if (POSITION_CONTROL) {
+         rho_error = rho_setopint - rho;
+         rho_dot_setpoint = Kp_rho*rho_error + Kd_rho*(rho_error - rho_error_old)/((float)period/1000);
+         rho_error_old = rho_error;
+   }
+
+   // Inner Control Loop
+   rho_dot_error = rho_dot_setpoint - rho_dot;
+   phi_dot_error = phi_dot_setpoint - phi_dot;
+
+   I_rho_dot = I_rho_dot + rho_dot_error*(float)period/1000.0;
+   I_phi_dot = I_phi_dot + phi_dot_error*(float)period/1000.0;
+
+   PWM_bar = Kp_rho_dot*rho_dot_error + Ki_rho_dot*I_rho_dot;
+   PWM_delta = Kp_phi_dot*phi_dot_error  + Ki_phi_dot*I_phi_dot;
+
+   M1PWM_value = (PWM_bar+PWM_delta)/2;
+   M2PWM_value = (PWM_bar-PWM_delta)/2;
+
+  if (M1PWM_value>0) {
+    digitalWrite(M1DIR,LOW);
+  }
+  else{
+    digitalWrite(M1DIR,HIGH);
+  }
+
+  if (M2PWM_value>0) {
+    digitalWrite(M2DIR,HIGH);
+  }
+  else{
+    digitalWrite(M2DIR,LOW);
+  }
+  
+  M1PWM_value = abs(M1PWM_value);
+  M2PWM_value = abs(M2PWM_value);
+
+  if (M1PWM_value>255) {
+    M1PWM_value =255;
+    I_rho_dot=0;
+    I_phi_dot=0;
+  }
+  if (M2PWM_value>255) {
+    M2PWM_value =255;
+    I_rho_dot=0;
+    I_phi_dot=0;
+  }
+  analogWrite(M1PWM, M1PWM_value);
+  analogWrite(M2PWM, M2PWM_value); 
+  
+   
+}
