9074d2f1330d87a60e2bfb0007ef189da7f77ced
samuelrbair <79220599+samuelrbair@users.noreply.github.com>
2021-04-23 11:40:44 -0600

    Add files via upload

---
+++ FinalDemo/HW2-1.ino
@@ -0,0 +1,47 @@
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x04
+int number = 0;
+int state = 0;
+int data[32] = {0};
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(115200); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+  Serial.println("Ready!");
+}
+
+void loop() {
+    delay(100);
+}
+
+// callback for received data
+void receiveData(int byteCount){
+    int i=0;
+    while(Wire.available()) {
+     data[i] = Wire.read();
+     Serial.print(data[i]);
+     Serial.print(' ');
+     i++;
+    }
+    //Serial.println(' ');
+    i--;
+    if (data[(i-1)] == 0){
+      number=data[i]+5;
+    }
+    if (data[(i-1)] == 1){
+      number=data[i]+10;
+    }
+}
+
+// callback for sending data
+void sendData(){
+    Wire.write(number);
+}
